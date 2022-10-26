d113355e2955621b97ac9e4cb736bf7cffeb32de
julianZeitler <julian.zeitler@zeiss.com>
2022-10-25 22:32:47 -0400

    DS2,3,4 Q1

--- Controller2.m
+++ Controller2.m
@@ -23,3 +23,30 @@ title('Example1: Closed-Loop Step Response.');
 zcl = zero(syscl2);
 pcl = pole(syscl2);
 syscl2;
+
+% DS2
+% the poles are
+%   -0.2175 + 0.0000i
+%   -0.0163 + 0.0129i
+%   -0.0163 - 0.0129i
+
+s = stepinfo(y,t);
+
+% settling time = 247.2102s
+% overshoot = 21.2206%
+% steady-state-error = 0.0003 = 0.03%
+
+numStep = 5;
+%denStep = [1 0];
+sysStep = tf(numStep);
+
+syscl2 = series(sysStep, syscl2);
+
+t = 0:0.5:500;
+y = step(syscl2,t);
+plot(t,y), grid;
+xlabel('Time (s)');
+ylabel('Output Y(t)');
+title('Example1: Closed-Loop Step Response.');
+zcl = zero(syscl2);
+pcl = pole(syscl2);
